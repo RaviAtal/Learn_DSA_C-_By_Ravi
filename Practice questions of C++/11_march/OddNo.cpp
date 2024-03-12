8756effb6e4a8bdc6ae833617ddc5c4c669c7dfe
Ravi <raviatal44@gmail.com>
2024-03-12 15:54:23 +0530

    odd no from 1 to 100

---
+++ Practice questions of C++/11_march/OddNo.cpp
@@ -0,0 +1,11 @@
+// Print all the odd numbers from 1 to 100.
+#include <iostream>
+using namespace std;
+
+int mani()
+{
+    for (int i = 1; i <= 100; i += 2)
+    {
+        cout << "Odd no From 1 to 100 are: " << i << endl;
+    }
+}
